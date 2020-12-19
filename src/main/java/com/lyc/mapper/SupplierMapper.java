beed89ee4c0844397d5bc9b96224e5a3c52bfe7b
liyanchao2tytp <57429651+liyanchao2tytp@users.noreply.github.com>
2020-12-19 12:35:21 +0800

    测试多套环境

--- src/main/java/com/lyc/mapper/SupplierMapper.java
+++ src/main/java/com/lyc/mapper/SupplierMapper.java
@@ -13,7 +13,7 @@ public interface SupplierMapper {
   @Select("SELECT * FROM t_supplier WHERE sup_id = #{id} ")
   Supplier findSupplierById(@Param("id") int id);
 
-  @Insert("INSERT INTO t_supplier(sup_name, sup_contact, sup_phone, adress) values(#{sup.supName}, #{sup.supContact}, #{sup.supPhone}, #{sup.adress})")
+  @Insert("INSERT INTO t_supplier(sup_name, sup_contact, sup_phone, adress) VALUES(#{sup.supName}, #{sup.supContact}, #{sup.supPhone}, #{sup.adress})")
   void addSupplier(@Param("sup") Supplier supplier);
 
   @Update("UPDATE t_supplier SET sup_name=#{supName}, sup_contact=#{supContact}, sup_phone=#{supPhone}, adress=#{adress} WHERE sup_id=#{id}")
