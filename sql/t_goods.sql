2eb1fcc1d57437f1496c81cb4493ca6adc294d94
L <934781829@qq.com>
2021-07-01 11:11:59 +0800

    配置文件乱码问题解决和数据库文件

---
+++ sql/t_goods.sql
@@ -0,0 +1,4 @@
+INSERT INTO `t_goods` (`goods_id`, `goods_name`, `goods_price`, `goods_num`, `sup_id`, `warn_num`, `is_delete`) VALUES (15, '上下', 4, 2, 1, 3, 0);
+INSERT INTO `t_goods` (`goods_id`, `goods_name`, `goods_price`, `goods_num`, `sup_id`, `warn_num`, `is_delete`) VALUES (16, 'we', 100, 1, 1, 20, 1);
+INSERT INTO `t_goods` (`goods_id`, `goods_name`, `goods_price`, `goods_num`, `sup_id`, `warn_num`, `is_delete`) VALUES (17, '达拉崩吧', 33, 2, 1, 38, 0);
+INSERT INTO `t_goods` (`goods_id`, `goods_name`, `goods_price`, `goods_num`, `sup_id`, `warn_num`, `is_delete`) VALUES (18, '沙琪玛', 33, 311, 3, 45, 0);
